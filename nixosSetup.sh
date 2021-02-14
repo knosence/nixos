8ca20bb9ae5131af707fa042f59ec034a67cba09
knosence <nadario.seay@gmail.com>
2021-02-14 18:21:19 +0000

    fixed bugs

--- nixosSetup.sh
+++ nixosSetup.sh
@@ -1,29 +1,33 @@
 
 #!/bin/bash
 
+read -p "Enter the Dirve (example /dev/sda:  " drive
+
 echo ">>> PARTITIONING DRIVES"
-parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart primary 512MiB -16GiB
-parted /dev/sda -- mkpart primary linux-swap -16GiB 100%
-parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB
-/dev/sda -- set 3 esp on
+parted $drive -- mklabel gpt
+parted $drive -- mkpart primary 512MiB -16GiB
+parted $drive -- mkpart primary linux-swap -16GiB 100%
+parted $drive -- mkpart ESP fat32 1MiB 512MiB
+sudo drive -- set 3 esp on
 
 echo ">>> FORMATING DRIVES"
-mkfs.ext4 -L nixos /dev/sda1
-mkswap -L swap /dev/sda2
-mkfs.fat -F 32 -n boot /dev/sda3
+mkfs.ext4 -L nixos ${drive}1
+mkswap -L swap ${drive}2
+mkfs.fat -F 32 -n boot ${drive}3
 
 echo ">>> MOUNTING DRIVES"
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
 mount /dev/disk/by-label/boot /mnt/boot
-swapon /dev/sda2
+swapon ${drive}2
 nixos-generate-config --root /mnt
 
-read -p ">>> OPENING CONFIG IN VIM"
+rm /mnt/etc/nixos/configuration.nix
+cp /home/nixos/nixos/configuration.nix /mnt/etc/nixos/configuration.nix
 
+read -p ">>> OPENING CONFIG IN VIM"
 vim /mnt/etc/nixos/configuration.nix
 
 read -p ">>> SETUP IS COMPLETE, HIT ENTER TO CONTINUE"
 
-
+nixos-install
